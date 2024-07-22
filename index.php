6f86cece3b920f61d068ad5f37d85ba267dddfcb
Vladimir0657305 <vv0657305@gmail.com>
2024-07-22 09:11:27 +0300

    Start

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+include_once ('controllers/home.php');
+include_once ('controllers/shop.php');
+include_once ('models/articles.php');
+include_once ('utils/session.php');
---
+++ models/articles.php
@@ -0,0 +1,11 @@
+<?php 
+
+class MArticles
+{
+
+}
+
+
+    // ['id'=>1, 'title'=> 'Article from db']
+    // ['id'=>2, 'title'=> 'Second article from db']
+    // ['id'=>3, 'title'=> 'Third article from db']
