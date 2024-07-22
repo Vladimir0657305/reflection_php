83fa9a16c84a4f590028cb73870317cb9e70161a
Vladimir0657305 <vv0657305@gmail.com>
2024-07-22 11:28:33 +0300

    Changes

--- controllers/shop.php
+++ controllers/shop.php
@@ -2,5 +2,24 @@
 
 class CShop
 {
+    protected MArticles $model;
 
+    public function __construct()
+    {
+        $this->model = new MArticles();
+        Session::set('test', mt_rand(1, 10));
+    }
+
+    public function run()
+    {
+        $articles = $this->model->all();
+        echo "<h1>Home page</h1>";
+
+        foreach ($articles as $art) {
+            echo "<hr><h2>{$art['title']}</h2>";
+        }
+
+        $val = Session::slice('test');
+        echo "<div>And other string - $val</div>";
+    }
 }
