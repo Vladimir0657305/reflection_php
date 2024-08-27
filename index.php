879b399b64d35562fd19837d7b977e31b4a9dac3
Vladimir0657305 <vv0657305@gmail.com>
2024-08-27 20:28:33 +0300

    Changes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include_once 'controllers/home.php';
 include_once 'controllers/shop.php';
 include_once 'models/articles.php';
 include_once 'utils/session.php';
-// include_once 'utils/logger.php';
+include_once 'utils/logger.php';
 
 class Container
 {
--- models/articles.php
+++ models/articles.php
@@ -1,6 +1,8 @@
 <?php
 
-class Articles
+include_once 'contracts/model.php';
+
+class Articles implements Model
 {
     protected Logger $logger;
 
@@ -8,10 +10,10 @@ class Articles
     {
         $this->logger = $logger;
     }
-    public function all()
+    public function all() : array
     {
         $this->logger->write("I am take all");
-        
+
         return [
             ['id' => 1, 'title' => "Article " . mt_rand(1, 10) . " from DB"],
             ['id' => 2, 'title' => "Article " . mt_rand(11, 20) . " from DB"],
